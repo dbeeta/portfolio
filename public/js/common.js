d068ed16856814bfa4073ad8e4dcabb046925062
MSAHIRULLAH <2019T00465@stu.cmb.ac.lk>
2022-12-20 13:57:30 +0530

    animation added

--- public/js/common.js
+++ public/js/common.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
         $(".img-bg-box").height($('.intro-img img').height());
         $(".project-div .lg-img").height($('.project-div .lg-img').width()/2);
 
+        $('.item').each(function() {
+            $('img', this).attr('src', "public/img/" + $('div', this).attr('data-url'));
+        });
+        $('.ui-design').each(function() {
+            $(this).attr('src', "public/img/" + $(this).attr('data-url'));
+        });
         $('.preview-img').each(function() {
             $(this).removeClass('preview-img');
         });
